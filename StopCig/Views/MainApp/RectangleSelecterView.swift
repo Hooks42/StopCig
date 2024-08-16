79ab850e8906951a0b9f211a04d75870d2c7f012
hooks42 <atgie.cedric2@gmail.com>
2024-08-16 13:10:03 +0200

    /ADD Creation des 3 Rectangles pour determiner la vue qu'on va afficher

---
+++ StopCig/Views/MainApp/RectangleSelecterView.swift
@@ -0,0 +1,18 @@
+//
+//  RectangleSelecterView.swift
+//  StopCig
+//
+//  Created by Hook on 16/08/2024.
+//
+
+import SwiftUI
+
+struct RectangleSelecterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RectangleSelecterView()
+}
---
+++ StopCig/Views/TestView/FillNightBlue.swift
@@ -0,0 +1,18 @@
+//
+//  FillNightBlue.swift
+//  StopCig
+//
+//  Created by Hook on 16/08/2024.
+//
+
+import SwiftUI
+
+struct FillNightBlue: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FillNightBlue()
+}
